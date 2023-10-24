10bba792dcb9ea9c72dca93bc1d9fb47b0f05b72
alendavies <alendavies10@gmail.com>
2023-10-24 19:33:56 -0300

    .

--- src/components/layout/NavItem.tsx
+++ src/components/layout/NavItem.tsx
@@ -6,7 +6,9 @@ type NavItemProps = {
 function NavItem(props: NavItemProps) {
     const { name } = props;
 
-    return <li className="hover:border-b-2 hover:border-primary hover:cursor-pointer h-full">{name}</li>;
+    return (
+        <li className="border-b-2 hover:border-primary cursor-pointer -pb-5 border-solid border-transparent">{name}</li>
+    );
 }
 
 export default NavItem;
--- src/pages/home/FeaturedMatch.tsx
+++ src/pages/home/FeaturedMatch.tsx
@@ -1,14 +1,14 @@
 function FeaturedMatch() {
     return (
         <div className="flex flex-col items-center bg-card p-6 mt-7">
-            <div className="flex flex-row space-x-2 items-center">
+            <div className="flex flex-row space-x-2 items-center mb-5">
                 <img src="public\images\premier.png" className="max-w-4 max-h-5" />
-                <p>Premier League</p>
+                <p className="font-semibold">Premier League</p>
             </div>
-            <p className="text-3xl mb-2">Mañana</p>
+            <p className="text-3xl mb-2 font-semibold">Mañana</p>
             <div className="flex w-full justify-around items-center">
                 <div className="w-1/2 flex-grow flex flex-row justify-start items-center">
-                    <img src="public\images\tottenham.png" className="absolute max-w-24 max-h-24" />
+                    <img src="public\images\tottenham.png" className="drop-shadow-lg absolute max-w-24 max-h-24" />
                     <div className="p-4 flex bg-blue-950 flex-grow pl-12 ml-12">
                         <p className="text-xl font-semibold">TOTTENHAM</p>
                     </div>
@@ -18,9 +18,10 @@ function FeaturedMatch() {
                     <div className="p-4 bg-black flex-grow flex justify-end pr-12 mr-12">
                         <p className="text-xl font-semibold">FULHAM</p>
                     </div>
-                    <img src="public\images\fulham.png" className="absolute max-w-24 max-h-24" />
+                    <img src="public\images\fulham.png" className="drop-shadow-lg absolute max-w-24 max-h-24" />
                 </div>
             </div>
+            <p className="font-semibold mt-7 text-sm">lunes, 23° octubre | 21:00 | Tottenham Hotspur Stadium</p>
         </div>
     );
 }
