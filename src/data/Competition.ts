a062b3e4f001447eec3b50b4a47d26467a6de529
alendavies <alendavies10@gmail.com>
2023-10-26 01:33:21 -0300

    feat: data test

---
+++ src/data/Competition.ts
@@ -0,0 +1,42 @@
+import { Competition } from "@/types/Competition";
+
+export const competitions: Competition[] = [
+    {
+        name: "Copa de la Liga Profesional",
+        country: "Argentina",
+        icon: "argentina.png",
+        teams: [
+            "Arsenal de Sarandí",
+            "Colón",
+            "Atlético Tucumán",
+            "Talleres",
+            "Belgrano",
+            "Central Córdoba",
+            "River Plate",
+            "Independiente"
+        ]
+    },
+    {
+        name: "Champions League",
+        country: "Europa",
+        icon: "champions.png",
+        teams: [
+            "Feyenoord",
+            "Lazio",
+            "FC Barcelona",
+            "Shakhtar",
+            "RB Leipzig",
+            "Estrella Roja",
+            "Celtic",
+            "Atlético de Madrid",
+            "Manchester City",
+            "Young Boys",
+            "PSG",
+            "AC Milan",
+            "Porto",
+            "Antwerp",
+            "Dortmund",
+            "Newcastle"
+        ]
+    }
+];
---
+++ src/data/Match.ts
@@ -0,0 +1,100 @@
+import { Match } from "@/types/Match";
+
+export const match: Match[] = [
+    {
+        local: { name: "Arsenal de Sarandí", icon: "arsenal.png" },
+        visitor: { name: "Colón", icon: "colon.png" },
+        local_score: 1,
+        visitor_score: 0,
+        league: "Copa de la Liga Profesional",
+        id: 1
+    },
+    {
+        local: { name: "Atlético Tucumán", icon: "tucuman.png" },
+        visitor: { name: "Talleres", icon: "talleres.png" },
+        local_score: 1,
+        visitor_score: 0,
+        league: "Copa de la Liga Profesional",
+        id: 2
+    },
+    {
+        local: { name: "Belgrano", icon: "belgrano.png" },
+        visitor: { name: "Central Córdoba", icon: "centralco.png" },
+        local_score: 1,
+        visitor_score: 1,
+        league: "Copa de la Liga Profesional",
+        id: 3
+    },
+    {
+        local: { name: "River Plate", icon: "river.png" },
+        visitor: { name: "Independiente", icon: "independiente.png" },
+        local_score: 3,
+        visitor_score: 0,
+        league: "Copa de la Liga Profesional",
+        id: 4
+    },
+    {
+        local: { name: "Feyenoord", icon: "feyenoord.png" },
+        visitor: { name: "Lazio", icon: "lazio.png" },
+        local_score: 3,
+        visitor_score: 1,
+        league: "Champions League",
+        id: 5
+    },
+    {
+        local: { name: "FC Barcelona", icon: "barcelona.png" },
+        visitor: { name: "Shakhtar", icon: "shakhtar.png" },
+        local_score: 2,
+        visitor_score: 1,
+        league: "Champions League",
+        id: 6
+    },
+    {
+        local: { name: "RB Leipzig", icon: "leipzig.png" },
+        visitor: { name: "Estrella Roja", icon: "estrellaroja.png" },
+        local_score: 3,
+        visitor_score: 1,
+        league: "Champions League",
+        id: 7
+    },
+    {
+        local: { name: "Celtic", icon: "celtic.png" },
+        visitor: { name: "Atlético de Madrid", icon: "atleti.png" },
+        local_score: 2,
+        visitor_score: 2,
+        league: "Champions League",
+        id: 8
+    },
+    {
+        local: { name: "Young Boys", icon: "youngboys.png" },
+        visitor: { name: "Manchester City", icon: "city.png" },
+        local_score: 1,
+        visitor_score: 3,
+        league: "Champions League",
+        id: 9
+    },
+    {
+        local: { name: "PSG", icon: "psg.png" },
+        visitor: { name: "AC Milan", icon: "milan.png" },
+        local_score: 3,
+        visitor_score: 0,
+        league: "Champions League",
+        id: 10
+    },
+    {
+        local: { name: "Antwerp", icon: "antwerp.png" },
+        visitor: { name: "Porto", icon: "porto.png" },
+        local_score: 1,
+        visitor_score: 4,
+        league: "Champions League",
+        id: 11
+    },
+    {
+        local: { name: "Newcastle", icon: "newcastle.png" },
+        visitor: { name: "Dortmund", icon: "dortmund.png" },
+        local_score: 0,
+        visitor_score: 1,
+        league: "Champions League",
+        id: 12
+    }
+];
