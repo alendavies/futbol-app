361b94028416d9536f1da82c1c5cb93e48fb9766
lazcanoluca <lazcanoluca@gmail.com>
2023-11-11 15:25:30 -0300

    feat: add basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages";
+import Layout from "./components/layout/Layout";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const routes = [
+    {
+        path: "/futbol-app",
+        element: <Layout />,
+        children: [
+            {
+                path: "",
+                element: <Home />
+            },
+            {
+                path: "matches",
+                element: <>matches</>
+            },
+            {
+                path: "matches/:matchId",
+                element: <>some match</>
+            },
+            {
+                path: "competitions",
+                element: <>competitions</>
+            },
+            {
+                path: "competitions/:competitionId",
+                element: <>some competition1</>
+            },
+            {
+                path: "teams",
+                element: <>teams</>
+            },
+            {
+                path: "teams/:teamId",
+                element: <>some team</>
+            }
+        ]
+    }
+];
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
