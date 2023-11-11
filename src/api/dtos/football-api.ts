7b32ed2d78a2a068e62e704ddf4198b4d106c7fa
lazcanoluca <lazcanoluca@gmail.com>
2023-11-10 23:17:16 -0300

    api methods in xonstructor

--- src/api/dtos/football-api.ts
+++ src/api/dtos/football-api.ts
@@ -1,4 +1,7 @@
-import { AxiosResponse } from 'axios'
+import { axiosInstance } from '@/lib/axios';
+import { AxiosError, AxiosResponse } from 'axios';
+import { TeamDTO } from './team';
+import { GetTeamsParams } from '../teams';
 
 /**
  * Represents the response body of a football API request.
@@ -6,11 +9,27 @@ import { AxiosResponse } from 'axios'
  * @property {string} get -
  */
 export type BodyResponse<TResponse> = {
-    get: string
-    parameters: any
-    errors: any
-    results: number
-    response: Array<TResponse>
-}
+    get: string;
+    parameters: any;
+    errors: any;
+    results: number;
+    response: Array<TResponse>;
+};
 
-export type Response<TResponse> = AxiosResponse<BodyResponse<TResponse>>
+export type Response<TResponse> = AxiosResponse<BodyResponse<TResponse>>;
+
+const getResponse = <TResponse>(response: AxiosResponse): TResponse[] => {
+    return response.data.response;
+};
+
+const getError = (error: AxiosError) => {
+    return Promise.reject(error.response?.data);
+};
+
+export const FootballAPI = {
+    getTeams: (params: GetTeamsParams) =>
+        axiosInstance
+            .get('/teams', { params })
+            .then(getResponse<TeamDTO>)
+            .catch(getError)
+};
